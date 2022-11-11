0642f864d76ab3e2df3ab618023a5da376abb8b5
Marek Slowinski <marekslowinski@gmail.com>
2022-11-11 17:08:40 +0100

    render function added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 {
+    const tasks = [
+        {
+            content: "",
+            done: false,
+        }
+
+    ];
+
+const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks){
+        htmlString += `
+        <li>
+            ${task.content}
+        </li>
+        `;
+    }
+};
+
     const init = () => {
 
     }
