ec3354156f2d73ae363b9e4d0e68b1526da0ab16
Marek Slowinski <marekslowinski@gmail.com>
2022-11-11 17:54:34 +0100

    onFormSubmit function added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,43 @@
 {
-    const tasks = [
-        {
-            content: "",
-            done: false,
-        }
-
-    ];
+    const tasks = [];
 
 const render = () => {
     let htmlString = "";
 
-    for (const task of tasks){
+    for (const task of tasks) {
         htmlString += `
         <li>
             ${task.content}
         </li>
         `;
     }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
 };
 
-    const init = () => {
+const addNewTask = (newTaskContent) => {
+    tasks.push({
+        content: newTaskContent
+    });
+    render();
+}
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === ""){
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
     }
 
         init();
