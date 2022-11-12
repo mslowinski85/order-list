bc30236f94e7ff8b8c20f4b06f6e1cceb1e17045
Marek Slowinski <marekslowinski@gmail.com>
2022-11-12 12:28:33 +0100

    remove buttons action added

--- js/script.js
+++ js/script.js
@@ -6,16 +6,34 @@ const render = () => {
 
     for (const task of tasks) {
         htmlString += `
-        <li class="list__item">
-            <button class="list__buttonDone"></button>
+       
+        <li class="list__item"
+            ${task.done ? "class=list__item--done" : ""}
+        >
+            <button class="list__button js-done"></button>
             ${task.content} 
-            <button class="list__buttonDelete"></button>
-            <hr>
+            <button class="list__button list__button--delete js-remove"></button> 
         </li>
+      
+        <hr>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const doneButton = document.querySelector(".js-done");
+
+    // doneButton.classList.toggle("list__item--done");
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            tasks.splice(index, 1);
+            render();
+        });
+        
+    });
 };
 
 const addNewTask = (newTaskContent) => {
